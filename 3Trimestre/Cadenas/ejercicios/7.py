06fa23cbb5bcb307ae1da1c7ff141fe1a072e159
LordBrandon07 <brandonnrodriguez07@gmail.com>
2023-02-20 22:36:23 -0500

    cadena7

--- 3Trimestre/Cadenas/ejercicios/7.py
+++ 3Trimestre/Cadenas/ejercicios/7.py
@@ -25,38 +25,3 @@ def tipos(cadena):
     print('vocales: ',vocales,'\nvocales con tilde: ',tilde,'\nconsonantes: ',consonantes,'\ncaracteres: ',caracteres)
 
 tipos(cadena)
-
-
-
-
-
-
-
-
-
-
-
-
-# def contarVocales(cad):
-#     vocales = 0
-#     consonantes = 0
-#     vocalesConTilde = 0
-#     caracterE = 0
-    
-#     cad = cad.lower()
-    
-#     for c in cad:
-#         if c.isalpha():
-#             if c in "aeiou":
-#                 vocales += 1
-#             elif c in "áéíóú":
-#                 vocalesConTilde += 1
-#             else:
-#                 consonantes += 1
-          
-#         if c not in "abcdefghijklmnñopqrstuvwxyzáéíóú":
-#             caracterE += 1
-
-#     print(vocales,consonantes,vocalesConTilde,caracterE)
-
-# contarVocales('caminar')
